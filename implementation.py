cd7401f4342e60594ef7179171c19263b1f4ca36
Luke Guerdan <lguerdan@yahoo.com>
2016-11-21 17:03:59 -0600

    add working nieve path traversal

--- implementation.py
+++ implementation.py
@@ -4,6 +4,7 @@
 # Feel free to use this code in your own projects, including commercial projects
 # License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 from __future__ import print_function
+import collections
 
 class SimpleGraph:
     def __init__(self):
@@ -21,7 +22,6 @@ example_graph.edges = {
     'E': ['B']
 }
 
-import collections
 
 class Queue:
     def __init__(self):
@@ -62,8 +62,8 @@ def draw_grid(graph, width=2, **style):
             print("%%-%ds" % width % draw_tile(graph, (x, y), style, width), end="")
         print()
 
-# data from main article
-DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
+# # data from main article
+# DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
 
 class SquareGrid:
     def __init__(self, width, height):
@@ -86,98 +86,98 @@ class SquareGrid:
         results = filter(self.passable, results)
         return results
 
-class GridWithWeights(SquareGrid):
-    def __init__(self, width, height):
-        super().__init__(width, height)
-        self.weights = {}
+# class GridWithWeights(SquareGrid):
+#     def __init__(self, width, height):
+#         super(SquareGrid, self).__init__(width, height)
+#         self.weights = {}
 
-    def cost(self, from_node, to_node):
-        return self.weights.get(to_node, 1)
+#     def cost(self, from_node, to_node):
+#         return self.weights.get(to_node, 1)
 
-diagram4 = GridWithWeights(10, 10)
-diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
-diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
-                                       (4, 3), (4, 4), (4, 5), (4, 6),
-                                       (4, 7), (4, 8), (5, 1), (5, 2),
-                                       (5, 3), (5, 4), (5, 5), (5, 6),
-                                       (5, 7), (5, 8), (6, 2), (6, 3),
-                                       (6, 4), (6, 5), (6, 6), (6, 7),
-                                       (7, 3), (7, 4), (7, 5)]}
+diagram4 = SquareGrid(10, 10)
+# diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
+# diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
+#                                        (4, 3), (4, 4), (4, 5), (4, 6),
+#                                        (4, 7), (4, 8), (5, 1), (5, 2),
+#                                        (5, 3), (5, 4), (5, 5), (5, 6),
+#                                        (5, 7), (5, 8), (6, 2), (6, 3),
+#                                        (6, 4), (6, 5), (6, 6), (6, 7),
+#                                        (7, 3), (7, 4), (7, 5)]}
 
-import heapq
+# import heapq
 
-class PriorityQueue:
-    def __init__(self):
-        self.elements = []
+# class PriorityQueue:
+#     def __init__(self):
+#         self.elements = []
 
-    def empty(self):
-        return len(self.elements) == 0
+#     def empty(self):
+#         return len(self.elements) == 0
 
-    def put(self, item, priority):
-        heapq.heappush(self.elements, (priority, item))
+#     def put(self, item, priority):
+#         heapq.heappush(self.elements, (priority, item))
 
-    def get(self):
-        return heapq.heappop(self.elements)[1]
+#     def get(self):
+#         return heapq.heappop(self.elements)[1]
 
-def dijkstra_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
+# def dijkstra_search(graph, start, goal):
+#     frontier = PriorityQueue()
+#     frontier.put(start, 0)
+#     came_from = {}
+#     cost_so_far = {}
+#     came_from[start] = None
+#     cost_so_far[start] = 0
 
-    while not frontier.empty():
-        current = frontier.get()
+#     while not frontier.empty():
+#         current = frontier.get()
 
-        if current == goal:
-            break
+#         if current == goal:
+#             break
 
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost
-                frontier.put(next, priority)
-                came_from[next] = current
+#         for next in graph.neighbors(current):
+#             new_cost = cost_so_far[current] + graph.cost(current, next)
+#             if next not in cost_so_far or new_cost < cost_so_far[next]:
+#                 cost_so_far[next] = new_cost
+#                 priority = new_cost
+#                 frontier.put(next, priority)
+#                 came_from[next] = current
 
-    return came_from, cost_so_far
+#     return came_from, cost_so_far
 
-def reconstruct_path(came_from, start, goal):
-    current = goal
-    path = [current]
-    while current != start:
-        current = came_from[current]
-        path.append(current)
-    path.append(start) # optional
-    path.reverse() # optional
-    return path
+# def reconstruct_path(came_from, start, goal):
+#     current = goal
+#     path = [current]
+#     while current != start:
+#         current = came_from[current]
+#         path.append(current)
+#     path.append(start) # optional
+#     path.reverse() # optional
+#     return path
 
-def heuristic(a, b):
-    (x1, y1) = a
-    (x2, y2) = b
-    return abs(x1 - x2) + abs(y1 - y2)
+# def heuristic(a, b):
+#     (x1, y1) = a
+#     (x2, y2) = b
+#     return abs(x1 - x2) + abs(y1 - y2)
 
-def a_star_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
+# def a_star_search(graph, start, goal):
+#     frontier = PriorityQueue()
+#     frontier.put(start, 0)
+#     came_from = {}
+#     cost_so_far = {}
+#     came_from[start] = None
+#     cost_so_far[start] = 0
 
-    while not frontier.empty():
-        current = frontier.get()
+#     while not frontier.empty():
+#         current = frontier.get()
 
-        if current == goal:
-            break
+#         if current == goal:
+#             break
 
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
+#         for next in graph.neighbors(current):
+#             new_cost = cost_so_far[current] + graph.cost(current, next)
+#             if next not in cost_so_far or new_cost < cost_so_far[next]:
+#                 cost_so_far[next] = new_cost
+#                 priority = new_cost + heuristic(goal, next)
+#                 frontier.put(next, priority)
+#                 came_from[next] = current
 
-    return came_from, cost_so_far
+#     return came_from, cost_so_far
--- route_find.py
+++ route_find.py
@@ -1,19 +1,15 @@
-from implementation import *
+import collections
+
+# Not currently being used. Could store additional state in future if neccessary
 class GridNode:
    def __init__(self, x, y):
       self.x = x
       self.y = y
 
-class Obstical:
-   def __init__(self, location, velocity):
-      self.location = location
-      self.velocity = velocity
-
-
 class SquareGraph:
    def __init__(self, size):
-      self.edges = {}
       self.size = size
+      self.weight = {}
 
    # def neighbors(self, id):
    #      return self.edges[id]
@@ -22,51 +18,55 @@ class SquareGraph:
       (x, y) = position
       return 1 <= x <= self.size and 1 <= y <= self.size
 
-   # def passable(self, position):
-   #    return position not in self.walls
-
    def get_neighbors(self, position):
       (x, y) = position
       neighbors = [(x + 1, y), (x - 1, y), (x, y+ 1), (x, y - 1), (x - 1, y - 1), (x + 1, y + 1), (x-1, y + 1), (x + 1, y - 1)]
       neighbors = filter(self.in_graph, neighbors)
       return neighbors
 
-   # def grid_to_graph(self):
+   def get_cost(self, to_node, from_node):
+      return 1
 
-   #    for i in range(0, self.size):
-   #       edges[i] = {}
-
-   #       for j in range(0, self.size):
-   #          grid_node = GridNode(i,j)
-   #          edges[i][j] = grid_node
-
-   #          #add adjacent horizontal nodes
-   #          if i < self.size:
-   #             add adjacent horizontal nodes
-   #             (ij), (i+1, j)
-
-   #          #add adjacent vertical nodes
-   #          if j < self.size:
-   #             add adjacent vertical nodes
-   #             (ij), (i, j+1)
-
-   #          #add diagnol nodes
-   #          if (i < self.size and j < self.size):
-   #             (ij)(i+ 1, j+ 1)
-   #             (i+ 1,j) (i, j+ 1)
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
-def print_graph(graph):
+# We could probably just store obstical state here. Maybe add a .move() method to calculate next position each time interval
+class Obstical(SquareGraph): # inherits from square graph so we can use in_graph method
+   def __init__(self, location, velocity, graph):
+      self.location = location
+      self.velocity = velocity
+      #also state for t + 1 location here
+
+   def move(self):
+      '''
+      # obstical logic here.
+      1) Check graph size and boundaries (using in_graph method)
+      2) next position based on current velocity
+      3) We will also need to store state of t + 1 position to Check for collisions
+      '''
+
+# Helper function to show adjacency list of graph
+def print_adj_graph(graph):
    for i in range(1, graph.size + 1):
       for j in range(1, graph.size + 1):
 
          print str(i) + str(j) + ":  " + str(graph.get_neighbors((i, j)))
 
-graph = SquareGraph(5)
-print_graph(graph)
-
-
-import collections
+# Helper function to show visual state of graph
+def print_visual_graph(graph, agent, obstacle1=(-1,-1), obstacle2=(-1,-1)):
+   for i in range(1, graph.size + 1):
+      row_out = ""
+      for j in range(1, graph.size + 1):
+         if ((i, j) == agent):
+            row_out += " O "
+         elif ((i, j) == obstacle1 or (i, j) == obstacle2):
+            row_out += " X "
+         else:
+            row_out += " - "
+      print row_out
 
 class Queue:
     def __init__(self):
@@ -75,31 +75,106 @@ class Queue:
     def empty(self):
         return len(self.elements) == 0
 
-    def put(self, x):
+    def set(self, x):
         self.elements.append(x)
 
-    def get(self):
+    def next(self):
         return self.elements.popleft()
 
+import heapq
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
 
-def breadth_first_search_2(graph, start):
+    def isempty(self):
+      if (len(self.elements) == 0):
+         return 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+def breadth_first_search_modified(graph, start):
     # return "came_from"
-    frontier = Queue()
-    frontier.put(start)
+    boundary = Queue()
+    boundary.set(start)
     came_from = {}
     came_from[start] = None
 
-    while not frontier.empty():
-        current = frontier.get()
+    while not boundary.empty():
+        current = boundary.next()
         for next in graph.get_neighbors(current):
             if next not in came_from:
-                frontier.put(next)
+                boundary.set(next)
                 came_from[next] = current
 
     return came_from
 
-came_from = breadth_first_search_2(graph, (1,1))
-for key, val in came_from.items():
-   print key, val
 
-draw_grid(diagram4, width=3, point_to=came_from, start=(1, 4), goal=(7, 8))
+'''Here is a demo traversal technique using only bfs with the came_from attribute.
+We run BFS from the finish vertex and follow the came_from
+from the start until we hit none (the source/finish vertex). Not really optimal though because BFS will visit all nodes first'''
+
+def nieve_path_forward(graph, start, finish):
+   came_from = breadth_first_search_modified(graph, finish)
+
+   #Lets try swapping came from to go_to by exchanging keys and values
+   # go_to = dict((v,k) for k,v in came_from.iteritems())
+
+   agent_position = start
+   while agent_position != None:
+      print agent_position
+      print_visual_graph(graph, agent_position)
+      agent_position = came_from[agent_position]
+
+
+
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.isempty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.get_neighbors(current):
+            new_cost = cost_so_far[current] + graph.get_cost(next, current)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from
+
+
+graph = SquareGraph(9)
+start = (1,1)
+end = (8,9)
+nieve_path_forward(graph, start, end)
+print "#############################"
+
+
+# def informed_path_forward(graph, start, finish):
+#    came_from = a_star_search(graph, start, finish)
+
+#    agent_position = start
+#    while agent_position != None:
+#       print agent_position
+#       print_visual_graph(graph, agent_position)
+#       agent_position = came_from[agent_position]
+
+# graph = SquareGraph(9)
+# start = (1,1)
+# end = (8,9)
+# informed_path_forward(graph, start, end)
